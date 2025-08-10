ac8856a97846cce81204c6548c5a2378acf9583d
Ditaasekar <122968245+Ditaasekar@users.noreply.github.com>
2025-08-11 05:40:33 +0700

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
-app.use('/api/laporan', require('./laporanRoutes'));
+app.use('/api/laporan', require('./LaporanRoutes'));
 
 const PORT = process.env.PORT || 5000;
 
@@ -21,5 +21,6 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 module.exports = app;
+
 
 
